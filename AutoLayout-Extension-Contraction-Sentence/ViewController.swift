27be27ddb13527848c87b3fd113128081da1e546
ddd503 <skbsporta7@gmail.com>
2019-01-20 04:44:55 +0900

    fix layout and animation

--- AutoLayout-Extension-Contraction-Sentence/ViewController.swift
+++ AutoLayout-Extension-Contraction-Sentence/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var textViewContainer: UIView!
 
     @IBAction func moreRead(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.4) {
+        UIView.animate(withDuration: 0.3) {
             self.textViewContainer.isHidden.toggle()
             sender.setTitle(self.textViewContainer.isHidden ? "たたむ" : "もっと見る", for: .normal)
         }
