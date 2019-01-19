64cbc8bd0b1addbd214f57d1a06a1d20d8533af6
ddd503 <skbsporta7@gmail.com>
2019-01-20 03:04:32 +0900

    Initial commit

---
+++ AutoLayout-Extension-Contraction-Sentence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout-Extension-Contraction-Sentence
+//
+//  Created by kawaharadai on 2019/01/20.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
